d6647041985f2b147d6c4693c7d9897f9f92576a
adam.samec <adam.samec@gmail.com>
2021-10-01 17:30:23 +0200

    Add Accordion component scenario and some refactoring

--- src/ScenariosApp.tsx
+++ src/ScenariosApp.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import { FluentProvider, teamsLightTheme, Button, Menu, MenuTrigger, MenuList, MenuPopover, MenuGroup, MenuGroupHeader, MenuItem, MenuItemCheckbox, MenuItemRadio } from '@fluentui/react-components';
+import { FluentProvider, teamsLightTheme, Button, Menu, MenuTrigger, MenuList, MenuPopover, MenuGroup, MenuGroupHeader, MenuItem, MenuItemCheckbox, MenuItemRadio, Accordion, AccordionItem, AccordionHeader, AccordionPanel } from '@fluentui/react-components';
 
-const Home = () => {
-  React.useEffect(() => {
-    document.title = 'Fluent UI usage scenarios';
-     }, []);
-   
+const appTitle = 'Fluent UI usage scenarios';
+const appTitleSeparator = ' | ';
+
+const GoBackLink: React.FunctionComponent = () => {
 return (
-  <>
-  <h1>Fluent UI usage scenarios</h1>
-<nav>
-  <ul>
-    <li>
-
-    <Link to="/profile-menu">Profile menu</Link>
-    </li>
-  </ul>
-</nav>
-</>
+<Link to="/">Go back to main menu</Link>
 );
-}
+};
 
 const StatusSubmenu: React.FunctionComponent = () => {
 return (
@@ -41,13 +30,7 @@ return (
 }
 
 const ProfileMenu = () => {
-  React.useEffect(() => {
- document.title = 'Profile Menu | Fluent UI usage scenarios';
-  }, []);
-  
 return (
-  <>
-  <Link to="/">Go back to main menu</Link>
   <Menu>
     <MenuTrigger >
       <Button>Profile</Button>
@@ -72,16 +55,101 @@ return (
       </MenuList>
     </MenuPopover>
   </Menu>
+);  
+}
+
+const AccordionForm = () => {
+return (
+<Accordion defaultOpenItems={'personalDetails'}>
+<AccordionItem value="personalDetails">
+  <AccordionHeader>Personal details</AccordionHeader>
+  <AccordionPanel>
+    <label htmlFor="name">Name:</label><input type="text" id="name" name="name" />
+    <label htmlFor="email">Email:</label><input type="text" id="email" name="email" />
+  <div role="group" aria-label="Gender">
+<input type="radio" id="male" name="gender" value="male" /><label htmlFor="male">male</label>
+<input type="radio" id="female" name="gender" value="female" /><label htmlFor="female">female</label>
+  </div>
+  </AccordionPanel>
+</AccordionItem>
+<AccordionItem value="residence">
+  <AccordionHeader>Residence</AccordionHeader>
+  <AccordionPanel>
+  <label htmlFor="street">Street:</label><input type="text" id="street" name="street" />
+  <label htmlFor="city">City:</label><input type="text" id="city" name="city" />
+  <label htmlFor="country">Country:</label><input type="text" id="country" name="country" />
+  </AccordionPanel>
+</AccordionItem>
+<AccordionItem value="hobbies">
+  <AccordionHeader>Hobbies</AccordionHeader>
+  <AccordionPanel>
+  <div role="group" aria-label="Hobbies">
+<input type="checkbox" id="books" name="hobbies" value="books" /><label htmlFor="books">books</label>
+<input type="checkbox" id="sports" name="hobbies" value="sports" /><label htmlFor="sports">sports</label>
+<input type="checkbox" id="music" name="hobbies" value="music" /><label htmlFor="music">music</label>
+<input type="checkbox" id="travelling" name="hobbies" value="travelling" /><label htmlFor="travelling">travelling</label>
+  </div>
+  </AccordionPanel>
+</AccordionItem>
+</Accordion>
+);
+}
+
+const HomePage = () => {
+  React.useEffect(() => {
+    document.title = appTitle;
+     }, []);
+   
+return (
+  <>
+  <h1>Fluent UI usage scenarios</h1>
+<nav>
+  <ul>
+    <li>
+    <Link to="/profile-menu">Profile menu</Link>
+    </li>
+    <li>
+    <Link to="/accordion">Accordion</Link>
+    </li>
+  </ul>
+</nav>
+</>
+);
+}
+
+const ProfileMenuPage = () => {
+  React.useEffect(() => {
+ document.title = 'Profile Menu' + appTitleSeparator + appTitle;
+  }, []);
+
+return (
+  <>
+  <GoBackLink/>
+  <ProfileMenu />
   </>
 );  
+}
+
+const AccordionFormPage = () => {
+  React.useEffect(() => {
+ document.title = 'Accordion' + appTitleSeparator + appTitle;
+  }, []);
+
+return (
+  <>
+  <GoBackLink/>
+  <AccordionForm />
+</>
+);
 }
 
 export const ScenariosApp: React.FunctionComponent = () => {
   return (
     <FluentProvider theme={teamsLightTheme}>
       <Router>
-      <Route path="/" exact component={Home} />
-      <Route path="/profile-menu" exact component={ProfileMenu} />
+      <Route path="/" exact component={HomePage} />
+      <Route path="/profile-menu" exact component={ProfileMenuPage} />
+      <Route path="/accordion" exact component={AccordionFormPage} />
       </Router>
 </FluentProvider>
   );
