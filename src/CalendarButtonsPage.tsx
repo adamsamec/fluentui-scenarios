67ab7ed06c919b4b76811bd3f2969237b1ea3c57
adam.samec <adam.samec@gmail.com>
2021-10-29 18:20:22 +0200

    Add alert for the calendar Save event button click

--- src/CalendarButtonsPage.tsx
+++ src/CalendarButtonsPage.tsx
@@ -3,9 +3,13 @@ import { Button } from '@fluentui/react-components';
 import { appTitle, appTitleSeparator, GoBackLink } from './ScenariosApp';
 
   const CalendarButtons: React.FunctionComponent = () => {
+const onSaveButtonClick = React.useCallback(() => {
+alert('The calendar event would have been saved.');
+}, []);
+
   return (
     <>
-      <Button>save event</Button>
+      <Button onClick={onSaveButtonClick}>Save event</Button>
       <Button disabled>Delete event</Button>
       <Button disabledFocusable>Show upcoming events</Button>
       </>
